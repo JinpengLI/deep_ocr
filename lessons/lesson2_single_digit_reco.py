432dc386be2121d8c5f10777abc4df0b55e6ef53
root <mr.li.jinpeng@gmail.com>
2016-10-01 16:06:45 +0000

    add codes for lesson2

---
+++ lessons/lesson2_single_digit_reco.py
@@ -0,0 +1,19 @@
+>>> from sklearn import datasets
+>>> digits = datasets.load_digits()
+>>> print digits.data.shape
+digits.images[0]
+
+digits.target
+from sklearn import svm
+clf = svm.SVC(gamma=0.001, C=100.)
+clf.fit(digits.data[:-1], digits.target[:-1]) 
+clf.predict(digits.data[-1:])
+
+
+
+import matplotlib.pyplot as plt
+plt.figure(1, figsize=(3, 3))
+plt.imshow(digits.data[-1].reshape((8,8)), cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+
+digits.target
