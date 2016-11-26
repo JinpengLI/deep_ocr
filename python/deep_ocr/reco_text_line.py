fde10965fb8945ee77d1762462834ca71c1e1181
root <mr.li.jinpeng@gmail.com>
2016-11-26 20:54:50 +0000

    Add reco cache

--- python/deep_ocr/reco_text_line.py
+++ python/deep_ocr/reco_text_line.py
@@ -7,43 +7,50 @@ from deep_ocr.cv2_img_proc import PreprocessResizeKeepRatioFillBG
 from deep_ocr.utils import extract_peek_ranges_from_array
 from deep_ocr.utils import merge_peek_ranges
 
-class RecoCacheRanges(object):
-    def __init__(self, caffe_cls, image, char_set):
+class RectImageClassifier(object):
+    def __init__(self, caffe_cls, image, char_set,
+                 caffe_cls_width=64, caffe_cls_height=64):
         self.caffe_cls = caffe_cls
         self.image = image
         self.cache_res = {}
         self.char_set = char_set
+        self.caffe_cls_width = caffe_cls_width
+        self.caffe_cls_height = caffe_cls_height
 
-    def do(self, peek_ranges):
+    def do(self, rects, boundary):
+        rects_to_reco = []
+        for rect in rects:
+            key = (rect, boundary)
+            if key not in self.cache_res:
+                rects_to_reco.append(rect)
         image = self.image
-        height = image.shape[0]
         char_imgs = []
         crop_zeros = PreprocessCropZeros()
         resize_keep_ratio = PreprocessResizeKeepRatioFillBG(
             self.caffe_cls_width, self.caffe_cls_height,
             fill_bg=False, margin=4)
-        for i, peek_range in enumerate(peek_ranges):
-            x = peek_range[0]
-            y = 0
-            w = peek_range[1] - x
-            h = height
+        for i, rect in enumerate(rects_to_reco):
+            x, y, w, h = rect
             char_img = image[y:y+h+1, x:x+w+1]
             char_img = crop_zeros.do(char_img)
             char_img = resize_keep_ratio.do(char_img)
             char_imgs.append(char_img)
         np_char_imgs = np.asarray(char_imgs)
         output_tag_to_max_proba = self.caffe_cls.predict_cv2_imgs(np_char_imgs)
-        ocr_res = ""
         for i, item in enumerate(output_tag_to_max_proba):
-            cur_peek_range = peek_ranges[i]
+            cur_rect = rects_to_reco[i]
+            key = (cur_rect, boundary)
             if len(self.char_set["set"]) > 0:
                 for char_p in item:
                     if char_p[0] in self.char_set["set"]:
-                        ocr_res += char_p[0]
-                        self.cache_res[cur_peek_range] = char_p[0]
+                        self.cache_res[key] = char_p[0]
                         break
             else:
-                ocr_res += item[0][0]
+                self.cache_res[key] = item[0][0]
+        ocr_res = ""
+        for rect in rects:
+            key = (rect, boundary)
+            ocr_res += self.cache_res[key]
         return ocr_res
 
 
@@ -54,48 +61,44 @@ class RecoTextLine(object):
                  caffe_cls_height,
                  page_w=500,
                  char_set=set(),
-                 debug_path=None):
+                 debug_path=None,
+                 boundary=(0,0,0)):
         self.caffe_cls = caffe_cls
         self.char_set = char_set
         self.caffe_cls_width = caffe_cls_width
         self.caffe_cls_height = caffe_cls_height
         self.debug_path = debug_path
         self.page_w = page_w
+        self.boundary = boundary
 
     def do(self, img_line):
+        rect_img_clf = RectImageClassifier(
+            self.caffe_cls,
+            img_line,
+            self.char_set,
+            caffe_cls_width=self.caffe_cls_width,
+            caffe_cls_height=self.caffe_cls_height)
+
         height = img_line.shape[0]
         vertical_sum = np.sum(img_line, axis=0)
         char_w = self.page_w * self.char_set["width"]
         ## first segmentation
-        peek_ranges = extract_peek_ranges_from_array(vertical_sum,
-                                                     minimun_val=10,
-                                                     minimun_range=2)
+        peek_ranges = extract_peek_ranges_from_array(
+            vertical_sum,
+            minimun_val=10,
+            minimun_range=2)
+
         peek_ranges = merge_peek_ranges(peek_ranges, char_w)
-        char_imgs = []
-        crop_zeros = PreprocessCropZeros()
-        resize_keep_ratio = PreprocessResizeKeepRatioFillBG(
-            self.caffe_cls_width, self.caffe_cls_height,
-            fill_bg=False, margin=4)
-        for i, peek_range in enumerate(peek_ranges):
+        rects = []
+        for peek_range in peek_ranges:
             x = peek_range[0]
             y = 0
             w = peek_range[1] - x
             h = height
-            char_img = img_line[y:y+h+1, x:x+w+1]
-            char_img = crop_zeros.do(char_img)
-            char_img = resize_keep_ratio.do(char_img)
-            char_imgs.append(char_img)
-        np_char_imgs = np.asarray(char_imgs)
-        output_tag_to_max_proba = self.caffe_cls.predict_cv2_imgs(np_char_imgs)
-        ocr_res = ""
-        for item in output_tag_to_max_proba:
-            if len(self.char_set["set"]) > 0:
-                for char_p in item:
-                    if char_p[0] in self.char_set["set"]:
-                        ocr_res += char_p[0]
-                        break
-            else:
-                ocr_res += item[0][0]
+            rect = (x, y, w, h)
+            rects.append(rect)
+        ocr_res = rect_img_clf.do(rects, self.boundary)
+
         ## end end segmenetation
         print(ocr_res.encode("utf-8"))
         if self.debug_path is not None:
